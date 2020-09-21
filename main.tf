b4ffda46d777ef1c1982d0a36a5baf326060a616
sjimenez-nr <69168173+sjimenez-nr@users.noreply.github.com>
2020-09-21 12:57:56 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-provider "aws" {
-  version = "2.33.0"
+# provider "aws" {
+#   version = "2.33.0"
 
-  region = var.aws_region
-}
+#   region = var.aws_region
+# }
 
-provider "random" {
-  version = "2.2"
-}
+# provider "random" {
+#   version = "2.2"
+# }
 
-resource "random_pet" "table_name" {}
+# resource "random_pet" "table_name" {}
 
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
+# resource "aws_dynamodb_table" "tfc_example_table" {
+#   name = "${var.db_table_name}-${random_pet.table_name.id}"
 
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
+#   read_capacity  = var.db_read_capacity
+#   write_capacity = var.db_write_capacity
+#   hash_key       = "UUID"
 
-  attribute {
-    name = "UUID"
-    type = "S"
-  }
-}
+#   attribute {
+#     name = "UUID"
+#     type = "S"
+#   }
+# }
